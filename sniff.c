367e147140040eb08cdc1d9ef0fd9545c1ff0cc2
custiya <kjwjerry@naver.com>
2025-03-25 23:16:42 +0900

    add packet structure

--- sniff.c
+++ sniff.c
@@ -3,17 +3,89 @@
 #include <pcap.h>
 #include <arpa/inet.h>
 
-pcap_t *handle;
-char dev[] = "enp0s3";
-char errbuf[PCAP_ERRBUF_SIZE];
-struct bpf_program fp;
-char filter_exp[] = "tcp";
-bpf_u_int32 mask;
-bpf_u_int32 net;
-struct pcap_pkthdr header;
-const u_char *packet;
+/* Ethernet header */
+struct ethheader {
+    u_char  ether_dhost[6]; /* destination host address */
+    u_char  ether_shost[6]; /* source host address */
+    u_short ether_type;     /* protocol type (IP, ARP, RARP, etc) */
+  };
+  
+/* IP Header */
+struct ipheader {
+    unsigned char      iph_ihl:4, //IP header length
+                       iph_ver:4; //IP version
+    unsigned char      iph_tos; //Type of service
+    unsigned short int iph_len; //IP Packet length (data + header)
+    unsigned short int iph_ident; //Identification
+    unsigned short int iph_flag:3, //Fragmentation flags
+                       iph_offset:13; //Flags offset
+    unsigned char      iph_ttl; //Time to Live
+    unsigned char      iph_protocol; //Protocol type
+    unsigned short int iph_chksum; //IP datagram checksum
+    struct  in_addr    iph_sourceip; //Source IP address
+    struct  in_addr    iph_destip;   //Destination IP address
+};
+
+  /* TCP Header */
+struct tcpheader {
+    u_short tcp_sport;               /* source port */
+    u_short tcp_dport;               /* destination port */
+    u_int   tcp_seq;                 /* sequence number */
+    u_int   tcp_ack;                 /* acknowledgement number */
+    u_char  tcp_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->tcp_offx2 & 0xf0) >> 4)
+    u_char  tcp_flags;
+#define TH_FIN  0x01
+#define TH_SYN  0x02
+#define TH_RST  0x04
+#define TH_PUSH 0x08
+#define TH_ACK  0x10
+#define TH_URG  0x20
+#define TH_ECE  0x40
+#define TH_CWR  0x80
+#define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+    u_short tcp_win;                 /* window */
+    u_short tcp_sum;                 /* checksum */
+    u_short tcp_urp;                 /* urgent pointer */
+};
+
+/* Psuedo TCP header */
+struct pseudo_tcp
+{
+    unsigned saddr, daddr;
+    unsigned char mbz;
+    unsigned char ptcl;
+    unsigned short tcpl;
+    struct tcpheader tcp;
+    char payload[1500];
+};
+
+void got_packet(u_char *args, const struct pcap_pkthdr *header,
+    const u_char *packet)
+{
+    struct ethheader *eth = (struct ethheader *)packet;
+
+    if (ntohs(eth->ether_type) == 0x0800) { // 0x0800 is IP type
+        struct ipheader * ip = (struct ipheader *)
+                                (packet + sizeof(struct ethheader)); 
+
+        printf("       From: %s\n", inet_ntoa(ip->iph_sourceip));   
+        printf("         To: %s\n", inet_ntoa(ip->iph_destip));    
+    }
+}
 
 int main(){
+
+    pcap_t *handle;
+    char dev[] = "enp0s3";
+    char errbuf[PCAP_ERRBUF_SIZE];
+    struct bpf_program fp;
+    char filter_exp[] = "tcp";  
+    bpf_u_int32 mask;
+    bpf_u_int32 net;
+    struct pcap_pkthdr header;
+    const u_char *packet;
+
     if (pcap_lookupnet(dev, &net, &mask, errbuf) == -1) {
         fprintf(stderr, "Can't get netmask for device %s\n", dev);
         net = 0;
@@ -34,9 +106,9 @@ int main(){
         return(2);
     }
 
-    packet = pcap_next(handle,&header);
-    printf("Jacked a packet with length[%d] lens",header.len);
-    pcap_close(handle);
+    pcap_loop(handle, -1, got_packet, NULL);
+
+    pcap_close(handle);   //Close the handle
     return(0);
 }
 
